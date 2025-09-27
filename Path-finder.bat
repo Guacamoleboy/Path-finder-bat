1c7a136453d6ef43c4befe62c9aebc229c64de46
Guacamoleboy <cph-jl325@stud.ek.dk>
2025-09-27 23:41:41 +0200

    Admin Mode Update

--- Path-finder.bat
+++ Path-finder.bat
@@ -1,9 +1,25 @@
 @ECHO OFF
 
+:: ----------------------------------------------
+:: Path Finder by Guacamoleboy (Admin Update)
+:: ----------------------------------------------
+
+NET SESSION >NUL 2>&1
+
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO.
+    ECHO *** Administrator rettigheder kræves - forsøger at genstarte i admin mode...
+    ECHO.
+
+    :: Resets and turns on Admin Mode (Admin Prompt)
+    powershell -Command "Start-Process '%~f0' -ArgumentList '%*' -Verb RunAs"
+    EXIT /B
+)
+
 color 3
 
 ECHO.
-ECHO Path Finder by Guacamoleboy
+ECHO Path Finder by Guacamoleboy [ADMIN MODE]
 ECHO.
 ECHO ---------------------------
 
@@ -16,10 +32,11 @@ ECHO mkdir = Make 1 or more directories..
 ECHO.
 ECHO ---------------------------
 
-ECHO. 
+ECHO.
 ECHO Setting Path To The Dragged File: %1
 cd /d "%~dp1"
 ECHO.
+
 cmd /k
 
 :: Program by Guacamoleboy
